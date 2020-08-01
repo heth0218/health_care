699eac3d051c3f1a8cf4eda7e373cf1c61829ed7
heth0218 <54405324+heth0218@users.noreply.github.com>
2020-08-01 16:51:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const cors = require('cors');
 const app = express();
 
 connectDB();
 
 app.use(express.json({ extended: false }));
+app.use(cors())
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const PORT = process.env.PORT || 5000;
-
-app.get('/',async(req,res)=>{
-res.send("hello there ,ssup");
-})
 
 app.use('/api/user', require('./routes/users'));
 app.use('/api/doctor', require('./routes/doctor'));
