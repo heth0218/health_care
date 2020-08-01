3327779d1a5f5f0e76ef0bda81dc47180b3979f8
heth0218 <54405324+heth0218@users.noreply.github.com>
2020-08-02 01:24:01 +0530

    Update diary.js

--- routes/diary.js
+++ routes/diary.js
@@ -4,6 +4,59 @@ const Diary = require('../models/Diary');
 const User = require('../models/User');
 const Doctor = require('../models/Doctor')
 
+
+//ONCLICK OF OPEN DIARY IN PHONE SEND THE CONTACT
+router.get('/:contact', async (req, res) => {
+    try {
+        const user = await User.findOne({ contact: req.params.contact })
+
+        console.log(user)
+        await User.findOneAndUpdate(
+            { contact: req.params.contact },
+            { $set: { diary: true } },
+            { new: true });
+
+        const updatedUser = await User.findOne({ contact: req.params.contact });
+
+        res.status(200).send(updatedUser)
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+//ON THE WEB TO GET THE USER DETAILS
+router.get('/hello/diaryUser', async (req, res) => {
+    try {
+        const user = await User.findOne({ diary: true });
+        console.log(user)
+        if (!user) {
+            return res.status(404).send({ msg: "No user found" })
+        }
+        res.status(200).send(user);
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+//AFTER THE USER CLOSES THE DIARY
+router.post('/close', async (req, res) => {
+    try {
+        await User.findByIdAndUpdate(req.body.id,
+            { $set: { diary: false } },
+            { new: true });
+
+        const updatedUser = await User.findById(req.body.id);
+
+        res.status(200).send(updatedUser);
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
+})
+
 //POST diary daily text scores summary and entity list
 router.post('/', async (req, res) => {
     try {
@@ -22,13 +75,13 @@ router.post('/sendData', async (req, res) => {
     try {
         const { startDate, endDate, userId } = req.body
 
-        const doctor = await Doctor.findOne({ name: "Rushank Shah" });
+        const doctor = await Doctor.findOne({ name: "Soham Dave" });
         await Doctor.findByIdAndUpdate(
             doctor._id,
             { $set: { clientData: req.body } },
             { new: true })
 
-        const updatedDoctor = await Doctor.findOne({ name: "Rushank Shah" })
+        const updatedDoctor = await Doctor.findOne({ name: "Soham Dave" })
         console.log(updatedDoctor)
         res.status(200).send(updatedDoctor)
 
@@ -39,12 +92,13 @@ router.post('/sendData', async (req, res) => {
 
 //RENDER CLIENT DETAIL LIST ON DOCTORS PAGE
 
-router.get('/clientDetail', async (req, res) => {
+router.get('/doc/clientDetail', async (req, res) => {
 
     try {
-        const doctor = await Doctor.findOne({ name: 'Rushank Shah' });
+        const doctor = await Doctor.findOne({ name: "Soham Dave" });
 
         const { startDate, endDate, userId } = doctor.clientData;
+        console.log(doctor.clientData)
         const diaries = await Diary.find({
             user: userId,
             date: {
@@ -61,4 +115,4 @@ router.get('/clientDetail', async (req, res) => {
 
 
 
-module.exports = router
+module.exports = router;
