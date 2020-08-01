b4d6053cc6ae7c3fbc8b99224bcdb70e9076e6c7
heth0218 <54405324+heth0218@users.noreply.github.com>
2020-08-01 14:14:26 +0530

    Update diary.js

--- routes/diary.js
+++ routes/diary.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const Diary = require('../models/Diary');
 const User = require('../models/User');
 
+
+//POST diary daily text scores summary and entity list
 router.post('/', async (req, res) => {
     try {
         const { text, user, scores, Summary, ent_list } = req.body
@@ -14,5 +16,28 @@ router.post('/', async (req, res) => {
         res.status(500).send(error)
     }
 })
+
+//DATE WISE BRING DATA
+router.post('/:id', async (req, res) => {
+    try {
+        const { startDate, endDate } = req.body
+
+        const diaries = await Diary.find({
+            user: req.params.id,
+            date: {
+                $gte: new Date(`${startDate}`),//"2020-07-05"
+                $lte: new Date(`${endDate}`)//"2020-07-31"
+            }
+        })
+        console.log("diaries", diaries)
+
+        res.status(200).send(diaries);
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+
 
 module.exports = router
