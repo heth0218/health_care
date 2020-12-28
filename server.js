b43540fea7d0bc856002ab09c43857d727618f5a
heth0218 <hethgala@gmail.com>
2020-12-28 23:54:46 +0530

    Payment gateway with razorpay and spotify api

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use('/api/user', require('./routes/users'));
 app.use('/api/doctor', require('./routes/doctor'));
 app.use('/api/report', require('./routes/report'));
 app.use('/api/diary', require('./routes/diary'));
+app.use('/api/spotify', require('./routes/spotify'));
 
 
 app.listen(PORT, () => {
